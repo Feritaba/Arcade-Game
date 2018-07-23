01d9d0bf7cca27ec3d0def68d8688a9cfef60fb7
foroozan akhavan <foroozan.akhavan@gmail.com>
2018-07-23 11:16:54 -0500

    changed based on project review

--- js/app.js
+++ js/app.js
@@ -1,106 +1,109 @@
-// Enemies object
-var Enemy = function(x,y,speed) {
-
-    this.x = x;
-    this.y = y + 60;
-    this.speed = speed;
-    this.sprite = 'images/enemy-bug.png';
-    this.step = 101;
-    this.boundry = this.step * 5;
-    this.resetPos = -this.step;
-};
-
-// Update the enemy's position
-Enemy.prototype.update = function(dt) {
-
-    if (this.x < this.boundry) {
-        this.x += this.speed * dt;
-    } else {
-        this.x = this.resetPos;
+// Enemies class
+class Enemy {
+    constructor(x,y,speed) {
+        this.x = x;
+        this.y = y + 51;
+        this.speed = speed;
+        this.sprite = 'images/enemy-bug.png';
+        this.step = 101;
+        this.boundry = this.step * 5;
+        this.resetPos = -this.step;
     }
-};
 
-// Draw the enemy on the screen
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+    // Update the enemy's position
+    update(dt) {
 
-// Player class
-var FirstPlayer = function () {
-    this.sprite = 'images/char-horn-girl.png';
-    this.step = 101;
-    this.jump = 83;
-    this.startX = this.step * 2;
-    this.startY = (this.jump * 4) + 60;
-    this.x = this.startX;
-    this.y = this.startY;
-};
-
-// Draw the player on the screen
-FirstPlayer.prototype.render = function() {
-        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
-
-// Player moves with arrow keys
-FirstPlayer.prototype.handleInput = function(input) {
-    switch(input) {
-        case 'left' :
-            if (this.x > 0)
-                {this.x -= this.step;
-            }
-        break;
-        case 'right' :
-            if (this.x < this.step * 4) {
-                this.x += this.step;
-            }
-        break;
-        case 'up' :
-            if (this.y > 0) {
-                this.y -= this.jump;
-            }
-        break;
-        case 'down' :
-            if (this.y < this.jump * 4) {
-                this.y += this.jump;
-            }
-        break;
-    }
-};
-
-// Update the player's position
-FirstPlayer.prototype.update = function(){
-    for (let enemy of allEnemies) {
-        if ((this.y === enemy.y) && (enemy.x + enemy.step/2 > this.x
-            && enemy.x < this.x + this.step/2) ) {
-                this.reset();
+        if (this.x < this.boundry) {
+            this.x += this.speed * dt;
+        } else {
+            this.x = this.resetPos;
         }
     }
 
-    // Won the game alert
-    if ( this.y < 60 ) {
-        alert('You won!!');
-        this.reset();
+    // Draw the enemy on the screen
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
     }
 }
 
-// Reset the player's position
-FirstPlayer.prototype.reset = function() {
-    this.y = this.startY;
-    this.x = this.startX;
+// Player class
+class FirstPlayer {
+    constructor() {
+        this.sprite = 'images/char-horn-girl.png';
+        this.step = 101;
+        this.jump = 83;
+        this.startX = this.step * 2;
+        this.startY = (this.jump * 4) + 51;
+        this.x = this.startX;
+        this.y = this.startY;
+    }
+
+    // Draw the player on the screen
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
+
+    // Player moves with arrow keys
+    handleInput(input) {
+        switch(input) {
+            case 'left' :
+                if (this.x > 0)
+                    {this.x -= this.step;
+                }
+            break;
+            case 'right' :
+                if (this.x < this.step * 4) {
+                    this.x += this.step;
+                }
+            break;
+            case 'up' :
+                if (this.y > 0) {
+                    this.y -= this.jump;
+                }
+            break;
+            case 'down' :
+                if (this.y < this.jump * 4) {
+                    this.y += this.jump;
+                }
+            break;
+        }
+    }
+
+    // Update the player's position
+    update() {
+        for (let enemy of allEnemies) {
+            if ( (this.y === enemy.y) && (enemy.x + enemy.step/2 + 20 > this.x)
+                && (enemy.x < this.x + this.step/2) ) {
+                    this.reset();
+            }
+        }
+
+        // Won the game alert
+        if ( this.y < 55 ) {
+            alert('You won!!');
+            this.reset();
+        }
+    }
+
+    // Reset the player's position
+    reset() {
+        this.y = this.startY;
+        this.x = this.startX;
+    }
 }
 
 // Instantiate our objects (player and enemy)
 const player = new FirstPlayer();
-const enemy1 = new Enemy(-101,0, 350);
-const enemy2 = new Enemy(-101,83, 300);
-const enemy3 = new Enemy((-101*2.5), (83 * 2), 250);
-const enemy4 = new Enemy(-101,83, 400);
-const allEnemies = [];
-allEnemies.push(enemy1,enemy2,enemy3,enemy4);
+const allEnemies = [
+    new Enemy (-101,0, 350),
+    new Enemy(-101,83, 300),
+    new Enemy((-101*2.5), (83 * 2), 30),
+    new Enemy(-101,83, 400)
+    ];
 
 // Listens for key presses and sends the keys to your player
 document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
+    let allowedKeys = {
         37: 'left',
         38: 'up',
         39: 'right',
